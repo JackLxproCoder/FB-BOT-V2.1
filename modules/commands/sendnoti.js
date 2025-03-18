47a73f9d28e952fe095ff37c101bfb947269bdcd
JackLxproCoder <jacklxprocoder@gmail.com>
2025-03-18 09:59:16 +0800

    Add files via upload

--- modules/commands/sendnoti.js
+++ modules/commands/sendnoti.js
@@ -1,41 +1,111 @@
+/*
+» Có lỗi LH FB: fb.com/writtenbyjack
+*/
 module.exports.config = {
     name: "sendnoti",
-    version: "1.0.0",
+    version: "1.1.1",
     hasPermssion: 2,
-    credits: "Yan Maglinte",
-    description: "Sends a message to all groups and can only be done by the admin.",
-    usePrefix: true,
-    commandCategory: "message",
-    usages: "[Text]",
-    cooldowns: 5
+    credits: "N1002",
+    description: "Gửi tin nhắn đến tấy cả nhóm và reply để phản hồi",
+    commandCategory: "System",
+    usages: "text",
+    cooldowns: 2
+};
+request = require("request");
+fse = require("fs-extra");
+imageDownload = require("image-downloader");
+moment = require("moment-timezone");
+fullTime = () => moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss || DD/MM/YYYY");
+module.exports.run = async({ api,
+    event, Users }) => {
+    const { threadID: tid, messageID: mid, senderID: sid, attachments: atms, messageReply: mR, type, body, args } = event; 
+    const allTid = global.data.allThreadID || [];
+    const atm = await type == "message_reply" ? mR.attachments : atms.length != 0 ? atms : "nofile";
+    const content = !args[1] ? "chỉ có tệp" : body.slice(body.indexOf(args[1]));
+    if (!args[1] && atm == "nofile") return api.sendMessage(`‼️ Bạn chưa nhập nội dung`, tid, mid);
+    var msg = `📢 Notice from Admin: ${(await Users.getData(sid)).name}\n⏱ Time: ${fullTime()}\n\n📝 Content: ${content}\n\n» Reply to respond to Admin.`
+    const uwu = atm == "nofile" ? msg : {
+        body: msg,
+        attachment: await DownLoad(atm)
+    };
+var c1 = 0, c2 = 0;
+    for (var idT of allTid) {
+      var promise = new Promise (async(r1, r2) => {
+ await api.sendMessage(uwu, idT, async(e, i) => {
+   if (e) r2(++c2); else r1(++c1)
+      return global.client.handleReply.push({
+            name: this.config.name,
+            messageID: i.messageID,
+            author: sid,
+            type: "userReply"
+        })
+      });
+    })
+  }
+promise.then(async(r) => api.sendMessage(`✅ Sent message to {r} group`, tid, mid)).catch(async(err) => api.sendMessage(`❌ Can't send notifications to ${err} group`, tid, mid))
+};
+module.exports.handleReply = async({ api, event, handleReply: h, Users, Threads }) => {
+    const { threadID: tid, messageID: mid, senderID: sid, attachments: atms, body, type } = event;
+    const { ADMINBOT } = global.config;
+    switch (h.type) {
+        case "userReply": {
+            const atm = atms.length != 0 ? atms : "nofile";
+            var msg = `📩 Feedback from User ${(await Users.getData(sid)).name}\n🛡 Group: ${(await Threads.getData(tid)).threadInfo.threadName}\n⏱ Time: ${fullTime()}\n\n📝 Content: ${atm == "nofile" ? body : "Only files to you"}\n\n» Reply to respond to user.`
+            const uwu = atm == "nofile" ? msg : {
+                body: msg,
+                attachment: await DownLoad(atm)
+            };
+          var c1 = 0, c2 = 0;
+            for (var idA of ADMINBOT) {
+              var promise = new Promise (async(r1, r2) => {
+                await api.sendMessage(uwu, idA, async(e, i) => {
+     if (e) r2(++c2); else r1(++c1)
+                    return global.client.handleReply.push({
+                        name: this.config.name,
+                        messageID: i.messageID,
+                        author: h.author, idThread: tid, idMessage: mid, idUser: sid,
+                        type: "adminReply"
+                    })
+                });
+            });
+       }; 
+          promise.then(async(r1) => api.sendMessage(`📨 Responded to Admin ${(await Users.getData(h.author)).name} and ${+r1-1} Other admin`, tid, mid)).catch(async(err) => api.sendMessage(`❌  Could not respond to ${err} Admin`, tid, mid))
+            break;
+        };
+    case "adminReply": {
+        const atm = atms.length != 0 ? atms : "nofile";
+        var msg = `📩 Response from the admin ${(await Users.getData(sid)).name}\n⏱ Time: ${fullTime()}\n\n📝 Nội dung: ${atm == "nofile" ? body : "Only files to you"}\n\n» Reply to response to admin.`
+        const uwu = atm == "nofile" ? msg : {
+            body: msg,
+            attachment: await DownLoad(atm)
+        };
+        await api.sendMessage(uwu, h.idThread, async(e, i) => {
+            if (e) return api.sendMessage(`Error`, tid, mid);
+            else api.sendMessage(`📨 Đã phản hồi đến User ${(await Users.getData(h.idUser)).name} tại nhóm ${(await Threads.getData(h.idThread)).threadInfo.threadName}`, tid, mid)
+            return global.client.handleReply.push({
+                name: this.config.name,
+                messageID: i.messageID,
+                author: sid,
+                type: "userReply"
+            })
+        }, h.idMessage);
+        break;
+    };
+  }
 };
 
-module.exports.run = async ({ api, event, args }) => {
-    const threadList = await api.getThreadList(25, null, ['INBOX']);
-    let sentCount = 0;
-    const custom = args.join(' ');
-
-    async function sendMessage(thread) {
-        try {
-            await api.sendMessage(`› A message from the Admin:\n\n${custom}`, thread.threadID);
-            sentCount++;
-        } catch (error) {
-            console.error("Error sending a message:", error);
-        }
+const DownLoad = async(atm) => {
+    var arr = [];
+    for (var i = 0; i < atm.length; i++) {
+        const nameUrl = request.get(atm[i].url).uri.pathname
+        const namefile = atm[i].type != "audio" ? nameUrl : nameUrl.replace(/\.mp4/g, ".m4a");
+        const path = __dirname + "/cache/" + namefile.slice(namefile.lastIndexOf("/") + 1);
+        await imageDownload.image({
+            url: atm[i].url,
+            dest: path
+        });
+        arr.push(fse.createReadStream(path));
+        fse.unlinkSync(path);
     }
-
-    for (const thread of threadList) {
-        if (sentCount >= 20) {
-            break;
-        }
-        if (thread.isGroup && thread.name != thread.threadID && thread.threadID != event.threadID) {
-            await sendMessage(thread);
-        }
-    }
-
-    if (sentCount > 0) {
-        api.sendMessage(`› Sent the notification successfully.`, event.threadID);
-    } else {
-        api.sendMessage("› No eligible group threads found to send the message to.", event.threadID);
-    }
+    return arr;
 };
